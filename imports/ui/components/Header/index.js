80a47bfc5e59a8cffdfcc99f7f08119267e30398
corawym <corawym@gmail.com>
2017-11-05 22:31:50 -0800

    Create header component and container

---
+++ imports/ui/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { AppBar } from 'material-ui'
+
+const Header = () => (
+  <AppBar
+    title="redence"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+)
+
+export default Header
--- imports/ui/containers/Header/Header.js
+++ imports/ui/containers/Header/Header.js
@@ -1,99 +1,15 @@
-import React from 'react'
-import Avatar from 'material-ui/Avatar'
-import Chip from 'material-ui/Chip'
-import FontIcon from 'material-ui/FontIcon'
-import SvgIconFace from 'material-ui/svg-icons/action/face'
-import {blue300, indigo900} from 'material-ui/styles/colors'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-const styles = {
-  chip: {
-    margin: 4,
-  },
-  wrapper: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-}
+import { Header } from '../../components/Header'
 
-function handleRequestDelete() {
-  alert('You clicked the delete button.')
-}
-
-function handleTouchTap() {
-  alert('You clicked the Chip.')
-}
-
-export default class ChipExampleSimple extends Component {
-
+class Header extends Component {
+  state = {  }
   render() {
     return (
-      <div style={styles.wrapper}>
-
-        <Chip
-          style={styles.chip}
-        >
-          Text Chip
-        </Chip>
-
-        <Chip
-          onRequestDelete={handleRequestDelete}
-          onClick={handleTouchTap}
-          style={styles.chip}
-        >
-          Deletable Text Chip
-        </Chip>
-
-        <Chip
-          onClick={handleTouchTap}
-          style={styles.chip}
-        >
-          <Avatar src="images/uxceo-128.jpg" />
-          Image Avatar Chip
-        </Chip>
-
-        <Chip
-          onRequestDelete={handleRequestDelete}
-          onClick={handleTouchTap}
-          style={styles.chip}
-        >
-          <Avatar src="images/ok-128.jpg" />
-          Deletable Avatar Chip
-        </Chip>
-
-        <Chip
-          onClick={handleTouchTap}
-          style={styles.chip}
-        >
-          <Avatar icon={<FontIcon className="material-icons">perm_identity</FontIcon>} />
-          FontIcon Avatar Chip
-        </Chip>
-
-        <Chip
-          onRequestDelete={handleRequestDelete}
-          onClick={handleTouchTap}
-          style={styles.chip}
-        >
-          <Avatar color="#444" icon={<SvgIconFace />} />
-          SvgIcon Avatar Chip
-        </Chip>
-
-        <Chip onClick={handleTouchTap} style={styles.chip}>
-          <Avatar size={32}>A</Avatar>
-          Text Avatar Chip
-        </Chip>
-
-        <Chip
-          backgroundColor={blue300}
-          onRequestDelete={handleRequestDelete}
-          onClick={handleTouchTap}
-          style={styles.chip}
-        >
-          <Avatar size={32} color={blue300} backgroundColor={indigo900}>
-            MB
-          </Avatar>
-          Colored Chip
-        </Chip>
-      </div>
+      <Header />
     )
   }
 }
+
+export default Header
