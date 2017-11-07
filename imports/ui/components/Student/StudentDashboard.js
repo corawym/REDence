ec93a341ce86881d9d8419af71db623e90654519
corawym <corawym@gmail.com>
2017-11-07 14:41:48 -0800

    Style student information

--- imports/ui/components/Student/StudentDashboard.js
+++ imports/ui/components/Student/StudentDashboard.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Avatar } from 'material-ui';
+
+import './styles.css'
 
 
 const StudentDashboard = ({studentInfo, totalAttendancePercent}) => {
-  
+  console.log(studentInfo.fullName[0])
   return(
-    <div className="student-info-wrapper">
-      <div className="student-info-personal">
-        <h1>{studentInfo.fullName}</h1>
-        <h2>{studentInfo.program[0].title} student<span> (program start date - finish date)</span></h2>
+    <div className="student-info-container">
+      <div className="student-info-wrapper">
+        <Avatar size={80}>{studentInfo.fullName[0]}</Avatar>
+        <div className="student-info-personal">
+          <h1>{studentInfo.fullName}</h1>
+          <h2>{studentInfo.program[0].title} student<span> (program start date - finish date)</span></h2>
+        </div>
       </div>
 
       <div className="student-stat-wrapper">
         <div>
           <h3>total attendance</h3>
           <p>{totalAttendancePercent}%</p>
-          <p>Over 90% to pass</p>
+          <p>Need over 90% to pass</p>
         </div>
         <div>
           <h3>total school days</h3>
