72ba427fda1c11ba4913d1e2e3ce92cc238d148a
Mark <marx.affiliate@gmail.com>
2017-11-08 11:46:26 -0800

    Updated signup page to create default profile on register

--- imports/ui/containers/SignupContainer/Signup.js
+++ imports/ui/containers/SignupContainer/Signup.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
-import { Tabs, Tab } from "material-ui/Tabs";
+import { withTracker } from "meteor/react-meteor-data";
+
 import RaisedButton from "material-ui/RaisedButton";
 
 import "./styles.css";
 import StyledTextField from "../../components/TextField/TextField";
 
-export default class Signup extends Component {
+class Signup extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -16,77 +17,100 @@ export default class Signup extends Component {
     };
   }
 
-  handleChange = value => {
-    this.setState({
-      value: value
-    });
-  };
-
-  signUpStudent(e) {
-    e.preventDefault();
-    let email = e.target.studentEmail.value;
-    let pass = e.target.studentPassword.value;
-    Accounts.createUser({
-      email: email,
-      password: pass
-    });
-  }
-
   signUpTeacher(e) {
     e.preventDefault();
     let email = e.target.teacherEmail.value;
     let pass = e.target.teacherPassword.value;
+    let firstName = e.target.firstName.value;
+    let lastName = e.target.lastName.value;
+
     Accounts.createUser({
       email: email,
-      password: pass
+      password: pass,
+      profile: {
+        fullName: `${lastName}, ${firstName}`,        
+        role: 'teacher',
+        program: []
+      }
     });
   }
 
-  handleCode(e){
+  handleCode(e) {
     this.setState({
       code: e.target.value
     });
-  };
+  }
 
   render() {
+    const { currentUserId } = this.props;
+
+    if (currentUserId) {
+      return <Redirect to='/' />
+    } else {
+      null;
+    }
+
     return (
-      <Tabs
-        value={this.state.value}
-        onChange={this.handleChange}
-        className="signup-tabs"
-      >
-        <Tab label="Student" value="a" className="tab-container">
-          <form autoComplete="off" onSubmit={this.signUpStudent}>
-            <StyledTextField label="email" className="text-field" name="studentEmail" />
-              <br />
-            <StyledTextField label="password" type="password" name="studentPassword" />
+      <div className="tab-container">
+        <form autoComplete="off" onSubmit={this.signUpTeacher}>
+          <StyledTextField
+            label="First Name"
+            className="text-field"
+            name="firstName"
+          />
+          <br />
+          <StyledTextField
+            label="Last Name"
+            className="text-field"
+            name="lastName"
+          />
+          <br />
+          <StyledTextField
+            label="email"
+            className="text-field"
+            name="teacherEmail"
+          />
+          <br />
+          <StyledTextField
+            label="password"
+            type="password"
+            name="teacherPassword"
+          />
 
-            <Link to={`/signup`} className="links"> Forgot password? </Link>
+          <p> Teacher Code </p>
 
-            <Link to={`/`}><RaisedButton label="Sign up" primary={true} style={{ width: "95%" }} type="submit" /></Link>
+          <StyledTextField
+            onChange={e => this.handleCode(e)}
+            label="teacher code"
+            name="code"
+            value={this.state.code}
+          />
 
-            <Link to={`/signup`} className="links"> New user? </Link>
-          </form>
-        </Tab>
+          <Link to={`/signup`} className="links">
+            {" "}
+            Forgot password?{" "}
+          </Link>
 
-        <Tab label="Teacher" value="b" className="tab-container">
-          <form autoComplete="off" onSubmit={this.signUpTeacher}>
-            <StyledTextField label="email" className="text-field" name="teacherEmail" />
-              <br />
-            <StyledTextField label="password" type="password" name="teacherPassword" />
+          <RaisedButton
+            label="Sign up"
+            primary={true}
+            style={{ width: "95%" }}
+            type="submit"
+            disabled={this.state.code === "hi" ? false : true}
+          />
 
-            <p> Teacher Code </p>
-
-            <StyledTextField onChange={(e) => this.handleCode(e)} label="teacher code" name="code" value={this.state.code} />
-
-            <Link to={`/signup`} className="links"> Forgot password? </Link>
-
-            <Link to={`/`}><RaisedButton label="Sign up" primary={true} style={{ width: "95%" }} type="submit" disabled={this.state.code === "hi" ? false : true} /></Link>
-
-            <Link to={`/signup`} className="links"> New user? </Link>
-          </form>
-        </Tab>
-      </Tabs>
+          <Link to={`/signup`} className="links">
+            {" "}
+            New user?{" "}
+          </Link>
+        </form>
+      </div>
     );
   }
 }
+
+export default withTracker(() => {
+  return {
+    currentUserId: Meteor.userId()
+  };
+})(Signup);
