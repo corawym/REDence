42cd009b44f2db82ad1331a771e4351bcd5cf6a8
Mark <marx.affiliate@gmail.com>
2017-11-12 12:57:17 -0800

    Functional student register

--- imports/ui/components/Teacher/TeacherDashboard.js
+++ imports/ui/components/Teacher/TeacherDashboard.js
@@ -2,12 +2,15 @@ import React from 'react';
 import DashTable from '../Table/DashTable';
 import RaisedButton from 'material-ui/RaisedButton';
 import TeacherMailList from './TeacherMailList';
+import { RegisterContainer } from '../../containers/RegisterContainer'
+
 
 const TeacherDashboard = ({ handleClick, submitAttendance, allAttendance, attendanceSubmitted, updateAttendance,allTeacherMessages, confirmationEmail }) => {
   return (
     <div>
       <DashTable tableHeaderColumn={['Student','Attend','Late','Absence','Exception','Total']} allAttendance={allAttendance} handleClick={handleClick}/>
       <RaisedButton label="Submit" primary={true} onClick={attendanceSubmitted ? updateAttendance : submitAttendance}/>
+      <RegisterContainer />
       <TeacherMailList allTeacherMessages={allTeacherMessages} confirmationEmail={confirmationEmail}/>
     </div>
   )
