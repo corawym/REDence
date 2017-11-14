27bc3afdfe2fb70ab504a05718a047eaaab129ad
corawym <corawym@gmail.com>
2017-11-13 22:54:27 -0800

    Add programs selections on teachers dashboard

--- imports/ui/components/Teacher/TeacherDashboard.js
+++ imports/ui/components/Teacher/TeacherDashboard.js
@@ -23,20 +23,62 @@ const styles = {
     padding:'0 16px',
     marginBottom:'50px',
     width:'200px'
-  }
+  },
+  programButton: {
+    borderRadius:'18px', 
+    margin:'20px 0 20px 5px'
+  },
+  programLabelStyle: {
+    color:'white', 
+    fontFamily:'"Ubuntu", sans-serif',
+    fontSize:'0.7rem'
+  },
+  disabledLabelStyle: {
+    color:'#969696', 
+    fontFamily:'"Ubuntu", sans-serif',
+    fontSize:'0.7rem'
+  },
+  
 }
 
 const TeacherDashboard = ({ handleClick, submitAttendance, allAttendance, attendanceSubmitted, updateAttendance,allTeacherMessages, confirmationEmail}) => {
   return (
     <div>
 
-      
-      
       <div className="display-flex">
         <MarkAttendanceIcon color='#e2231a' style={{width:'50px', height:'50px'}}/>
         <h2 className='dashboard-section-name'>Attendance</h2>
       </div>
 
+      <div>
+        <FlatButton 
+          label="App Dev" 
+          labelStyle={styles.programLabelStyle}
+          style={styles.programButton}
+          backgroundColor='#313131'
+          hoverColor='#e2231a'
+        />
+        <FlatButton 
+          label="Web Dev" 
+          labelStyle={styles.disabledLabelStyle}
+          style={styles.programButton}  
+        />
+        <FlatButton 
+          label="UI design" 
+          labelStyle={styles.disabledLabelStyle}
+          style={styles.programButton}
+        />
+        <FlatButton 
+          label="UX design" 
+          labelStyle={styles.disabledLabelStyle}
+          style={styles.programButton}
+        />
+        <FlatButton 
+          label="Digital Marketing" 
+          labelStyle={styles.disabledLabelStyle}
+          style={styles.programButton}
+        />
+      </div>
       <DashTable tableHeaderColumn={['Student','Attend','Late','Absence','Exception','Total']} allAttendance={allAttendance} handleClick={handleClick}/>
       
       {attendanceSubmitted? false : <RegisterContainer />}
